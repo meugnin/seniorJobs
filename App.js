ede2c030f9fc828c85ff73db46b899f528093087
meugnin <jh_abdo@hotmail.com>
2021-08-30 20:44:16 -0300

    inicial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text,Image} from 'react-native';
+
+function App(){
+  return(
+    <View>
+    <Text style={{color: 'brown', fontSize: 25 , margin: 15}}>Olá troxas</Text>
+    <Image
+    source ={{uri: }}/>
+  </View>      
+  )
+}
+export default App;
